bc2d14e9737c04e2ca0a26bc3a37cc73e2d94d1f
binvant <binvantbajwa@gmail.com>
2022-02-28 16:28:27 -0500

    Added Procfile, requirements.txt for Hosting

--- Property/urls.py
+++ Property/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from django.conf.urls import url
+from django.urls import re_path as url
 
 urlpatterns = [
     path("", views.index, name="index"),
