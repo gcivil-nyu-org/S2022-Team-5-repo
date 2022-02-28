6e09e4041147516bc1852e6a03b70620b4612a2a
binvant <binvantbajwa@gmail.com>
2022-02-28 17:04:30 -0500

    Updated settings.py

--- HouseMe/settings.py
+++ HouseMe/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import django_heroku
-django_heroku.settings(locals())
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -122,3 +121,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+django_heroku.settings(locals())
