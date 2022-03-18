88c759d77b2e8019a6b1a70ca4417a5b33727335
Aditya Chavan <adityachavan198@gmail.com>
2022-03-18 19:30:07 -0400

    createlisting

--- Property/views.py
+++ Property/views.py
@@ -41,7 +41,32 @@ def createlistingform(request):
     return render(request, 'Property/createlistingform.html', payload)
 
 def createlisting(request):
-    pass
+    name = request.POST['listing_name']
+    address1 = request.POST['address1']
+    address2 = request.POST['address2']
+    city = request.POST['city']
+    state = request.POST['state']
+    borough = request.POST['borough']
+    zipcode = request.POST['zipcode']
+    latitude = request.POST['latitude']
+    latitude = request.POST['latitude']
+    bedrooms = request.POST['bedrooms']
+    bathrooms = request.POST['bathrooms']
+    area = request.POST['area']
+    rent = request.POST['rent']
+    furnished = request.POST['furnished']
+    elevator = request.POST['elevator']
+    heating = request.POST['heating']
+    parking = request.POST['parking']
+    laundry = request.POST['laundry']
+    map_url = request.POST['map_url']
+    photo_url = request.POST['photo_url']
+    matterport_link = request.POST['matterport_link']
+    description = request.POST['description']
+    owner = request.user
+    city = City.objects.get(name=city)
+    state = State.objects.get(name = state)
+
 
 def browselistings(request):
     listings = Listing.objects.all()
