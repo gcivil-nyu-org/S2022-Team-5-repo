84970ba9f5dc213fb2a865d392bcb20f96af8387
aaronk4311 <70861070+aaronk4311@users.noreply.github.com>
2022-03-19 22:45:37 -0400

    organized structure of project

--- Property/urls.py
+++ Property/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path("createlisting", views.createlisting, name="createlisting"),
     path("createlistingform", views.createlistingform, name="createlistingform"),
     path("browselistings", views.browselistings, name="browselistings"),
+
+    # TODO: use slug so new properties that are registered can be dynamically added
+    # path("<slug>", views.),
+    # path("test-property", )
 ]
