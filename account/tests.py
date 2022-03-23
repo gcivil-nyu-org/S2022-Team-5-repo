9ba3f8422dba34000ee89c61a0d49f012bfb92f3
aaronk4311 <70861070+aaronk4311@users.noreply.github.com>
2022-03-22 22:21:58 -0400

    added unit test for signup submit and removed some unused css files

--- account/tests.py
+++ account/tests.py
@@ -19,4 +19,15 @@ class TestAccountForms(TestCase):
         response = self.client.post(reverse("account:signupform"))
         self.assertEqual(response.status_code, 200)
     
-    
+    def testSignupSubmit(self):
+        response = self.client.post(reverse("account:signupsubmit"), 
+        data = {
+            "fname": self.firstName,
+            "lname": self.lastName,
+            "username": self.username,
+            "email": self.email,
+            "phone": self.phone,
+            "password": self.password,
+        })
+        
+        self.assertEqual(response.status_code, 200)
