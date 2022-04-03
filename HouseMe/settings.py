7b67d50827c72fef414012d9ca7a3d641eba873f
Yanjie Xu <yx2304@nyu.edu>
2022-04-03 18:08:50 -0400

    Installed django-crispy-forms

--- HouseMe/settings.py
+++ HouseMe/settings.py
@@ -38,9 +38,14 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "crispy_forms",
+    "crispy_bootstrap5",
     "Property",
     "account",
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
--- Property/views.py
+++ Property/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from .models import Listing
 from django.http import HttpResponseRedirect
-
+from .forms import ListingForm
+from django.http import JsonResponse
+from django.contrib.auth.decorators import login_required
 # TODO validate email
 # from django.core.validators import validate_email
 # from django.core.exceptions import ValidationError
@@ -95,3 +97,27 @@ def browselistings(request):
 
 def testproperty(request):
     return render(request, "Property/property_page.html")
+
+
+def newlisting(request):
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = ListingForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            form.save()
+            result = "Success"
+            message= "Your profile has been updated"
+        else:
+            result = "Failed"
+            message = "Failed to save listings form"
+        data = {'result': result, 'message': message}
+        return JsonResponse(data)
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = ListingForm()
+        context = {'form':form}
+    return render(request, 'property/newlisting.html', context)
+
