e339674748e712744a62f6194b2270ad1ad5de9b
binvant <binvantbajwa@gmail.com>
2022-05-10 16:59:23 -0400

    Update settings.py

--- HouseMe/settings.py
+++ HouseMe/settings.py
@@ -142,12 +142,14 @@ THUMBNAIL_ALIASES = {
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
+if DEBUG:
+        STATICFILES_DIRS = [
+            os.path.join(BASE_DIR, 'static')
+       ]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATIC_URL = "/static/"
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
-
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
 # after signup redirect to loginform
