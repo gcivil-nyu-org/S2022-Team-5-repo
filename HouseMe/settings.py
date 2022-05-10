7e169699a77ef7f885a4607527f24c0d3a3122e5
binvant <binvantbajwa@gmail.com>
2022-05-10 19:22:58 -0400

    Collectstatic

--- HouseMe/settings.py
+++ HouseMe/settings.py
@@ -26,11 +26,7 @@ SECRET_KEY = "9x7%k&%715*rf5ep56@_*vth0sf52hc2_1fysm27&odc&ee5n&"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [
-    "127.0.0.1",
-    "housieme.herokuapp.com",
-    "houseme-app.herokuapp.com",
-]
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
@@ -146,9 +142,10 @@ THUMBNAIL_ALIASES = {
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static"),
+# ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
