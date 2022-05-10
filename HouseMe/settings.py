bf169ce3bb86d88a25e04d8d72eb02baeabcec54
binvant <binvantbajwa@gmail.com>
2022-05-10 19:29:21 -0400

    Update settings.py

--- HouseMe/settings.py
+++ HouseMe/settings.py
@@ -145,6 +145,7 @@ THUMBNAIL_ALIASES = {
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # STATICFILES_DIRS = [
